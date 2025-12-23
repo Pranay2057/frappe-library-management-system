f648e21c7df03a9587f20a7ee612cebd991fe0ad
Pranay Gurung <pranay.gurung@frostdigi.ai>
2025-12-23 16:58:39 +0545

    Added library transaction controller

--- library_management/library_management/doctype/library_transaction/library_transaction.py
+++ library_management/library_management/doctype/library_transaction/library_transaction.py
@@ -1,9 +1,26 @@
 # Copyright (c) 2025, Pranay and contributors
 # For license information, please see license.txt
 
-# import frappe
+from unittest.loader import VALID_MODULE_NAME
+
+import frappe
+from frappe.model.delete_doc import DocStatus
 from frappe.model.document import Document
 
 
 class LibraryTransaction(Document):
-	pass
+	def before_submit(self):
+		pass
+
+	def validate_membership(self):
+		valid_membership = frappe.db.exists(
+			"Library Membership",
+			{
+				"library_member": self.library_member,
+				"docstatus": DocStatus.submitted(),
+				"from_date": ("<", self.date),
+				"to_date": (">", self.date),
+			},
+		)
+		if not valid_membership:
+			frappe.throw("The member doesn not have a valid membership")
