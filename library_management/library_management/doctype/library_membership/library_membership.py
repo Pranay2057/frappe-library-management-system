469c8a3f3c0f1e1eb71ac4dde9d108195763a3e3
Pranay Gurung <pranay.gurung@frostdigi.ai>
2025-12-23 11:45:52 +0545

    Added library membership and library transaction doctypes

---
+++ library_management/library_management/doctype/library_membership/library_membership.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2025, Pranay and contributors
+# For license information, please see license.txt
+
+# import frappe
+import frappe
+from frappe.model.docstatus import DocStatus
+from frappe.model.document import Document
+
+
+class LibraryMembership(Document):
+	def before_submit(self):
+		exists = frappe.db.exists(
+			"Library Membership",
+			{
+				"library_member": self.library_member,
+				"docstatus": DocStatus.submitted(),
+				"to_date": (">", self.from_date),
+			},
+		)
+		if exists:
+			frappe.throw("There is an active membership for this member")
