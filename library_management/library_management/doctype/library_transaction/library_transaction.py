d94d7ac5d33b3c8e69ed2c653808106b1f63fab7
Pranay Gurung <pranay.gurung@frostdigi.ai>
2025-12-25 11:31:04 +0545

    Added library settings doctype. Validation for max issues.

--- library_management/library_management/doctype/library_transaction/library_transaction.py
+++ library_management/library_management/doctype/library_transaction/library_transaction.py
@@ -12,25 +12,26 @@ class LibraryTransaction(Document):
 	def before_submit(self):
 		if self.type == "Issue":
 			self.validate_issue()
-			article=frappe.get_doc("Article", self.article)
+			self.validate_maximum_limit()
+			article = frappe.get_doc("Article", self.article)
 			article.status = "Issued"
 			article.save()
 
 		if self.type == "Return":
 			self.validate_return()
-			article=frappe.get_doc("Article", self.article)
+			article = frappe.get_doc("Article", self.article)
 			article.status = "Available"
 			article.save()
 
 	def validate_issue(self):
 		self.validate_membership()
 		article = frappe.get_doc("Article", self.article)
-		if article.status = "Issued":
+		if article.status == "Issued":
 			frappe.throw("Article is already issued to another member")
 
 	def validate_return(self):
 		article = frappe.get_doc("Article", self.article)
-		if article.status = "Available":
+		if article.status == "Available":
 			frappe.throw("Article cannot be returned without being issued first")
 
 	def validate_membership(self):
@@ -45,3 +46,16 @@ class LibraryTransaction(Document):
 		)
 		if not valid_membership:
 			frappe.throw("The member does not have a valid membership")
+
+	def validate_maximum_limit(self):
+		max_articles = frappe.db.get_single_value("Library Settings", "max_articles")
+		count = frappe.db.count(
+			"Library Transaction",
+			{
+				"library_member": self.library_member,
+				"type": "Issue",
+				"docstatus": DocStatus.submitted(),
+			},
+		)
+		if count >= max_articles:
+			frappe.throw("Maximum limit reached for issuing articles")
