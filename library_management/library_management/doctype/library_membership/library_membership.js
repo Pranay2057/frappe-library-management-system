04b1512135be71ae5d8793a7a78fb38fc70f2b63
Pranay Gurung <pranay.gurung@frostdigi.ai>
2025-12-27 07:11:21 +0545

    Added custom js for library membership

--- library_management/library_management/doctype/library_membership/library_membership.js
+++ library_management/library_management/doctype/library_membership/library_membership.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2025, Pranay and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Library Membership", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Library Membership", {
+	refresh(frm) {
+		frm.dashboard.add_comment("🟢 CUSTOM APP LOADED - Build Working!", "green", true);
+	},
+});
